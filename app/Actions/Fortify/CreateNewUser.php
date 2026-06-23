8d9d06b88da4841c6204e6bc6a309a0bca5893ec
kishi tetsuya <stojkovic7110@icloud.com>
2026-06-24 04:42:56 +0900

    4.CreateNewUser::create() の Validator::make() に messages() を追加(3はFortify::createUsersUsing(CreateNewUser::class) が既に登録されていたため省略)

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,22 +22,44 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
-        Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => [
-                'required',
-                'string',
-                'email',
-                'max:255',
-                Rule::unique(User::class),
+        Validator::make(
+            $input,
+            [
+                'name' => ['required', 'string', 'max:255'],
+                'email' => [
+                    'required',
+                    'email',
+                    'max:255',
+                    Rule::unique(User::class),
+                ],
+                'password' => $this->passwordRules(),
             ],
-            'password' => $this->passwordRules(),
-        ])->validate();
+            $this->messages(),
+        )->validate();
 
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+    }
+
+    private function messages(): array
+    {
+        return [
+            'name.required' => 'お名前を入力してください',
+            'name.string' => 'お名前は文字列で入力してください',
+            'name.max' => 'お名前は255文字以内で入力してください',
+
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'email.max' => 'メールアドレスは255文字以内で入力してください',
+            'email.unique' => 'そのメールアドレスは既に使用されています',
+
+            'password.required' => 'パスワードを入力してください',
+            'password.string' => 'パスワードは文字列で入力してください',
+            'password.min' => 'パスワードは8文字以上で入力してください',
+            'password.confirmed' => 'パスワードと一致しません',
+        ];
     }
 }
